27d722ed34d9ffd9aac84b54eb143bbcb966ac1c
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-06-17 15:06:17 -0400

    Create importer.py

---
+++ importer.py
@@ -0,0 +1,6 @@
+def view(app):
+  import requests
+  response = requests.get('http://raw.githack.com/Mayank-1234-cmd/pyInDeepak/master/' + app + '.py')
+
+response
+#If doesn't work then response.content
