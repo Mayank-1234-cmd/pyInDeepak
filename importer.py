7ae37ce2adbbecf9d5c46f00ed2fe660d2a8a6ff
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-06-18 19:25:05 -0400

    Update importer.py

--- importer.py
+++ importer.py
@@ -1,6 +1,5 @@
 def view(app):
   import requests
   response = requests.get('http://raw.githack.com/Mayank-1234-cmd/pyInDeepak/master/' + app + '.py')
-
-response
+  response
 #If doesn't work then response.content
